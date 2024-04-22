d4861963f855e06043fb5ed54a29704a79f3a970
fahrenghate <128506486+fahrenghate@users.noreply.github.com>
2024-04-23 00:33:12 +0300

    Update main.py
    Добавлено качество воздуха

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, Location
 BOT_TOKEN = "7193872414:AAFA_aW7LzLKppy5fSZ-iYVW6A0fy0aMSgw"
 OPENWEATHERMAP_API_KEY = "6df40c91c04d29efe8ab5a85c0d29e51"
 OPENWEATHERMAP_API_URL = "http://api.openweathermap.org/data/2.5/weather"
-
+AIR_API_URL = 'https://api.api-ninjas.com/v1/airquality'
+AIR_API_KEY = 'xy3f78YBcCE5/DNQW/dqKA==Op4Zzd3SCjm5IRtK'
 cloudiness_translation = {
     'clear sky': 'Ясно',
     'light rain': 'Маленький дождь',
@@ -46,42 +47,52 @@ async def get_weather(message: types.Message):
     if message.content_type == types.ContentType.TEXT:
         city = message.text.capitalize()
 
-        params = {
+        weather_params = {
             'q': city,
             'appid': OPENWEATHERMAP_API_KEY, 
             'units': 'metric'
+        }
+        air_params = {
+            'city': city
         }
     else:  # Если пользователь отправил локацию
         latitude = message.location.latitude
         longitude = message.location.longitude
 
-        params = {
+        weather_params = {
             'lat': latitude,
             'lon': longitude,
             'appid': OPENWEATHERMAP_API_KEY,
             'units': 'metric'
         }
+        air_params = {
+            'lat': latitude,
+            'lon': longitude
+        }
 
-    response = requests.get(OPENWEATHERMAP_API_URL, params=params)
+    response = requests.get(OPENWEATHERMAP_API_URL, params=weather_params)
     weather_data = response.json()
-
-    if response.status_code == 200:
+    air_response = requests.get(AIR_API_URL, headers={'X-Api-Key': AIR_API_KEY}, params=air_params)
+    air_data = air_response.json()
+    if response.status_code == 200 and air_response.status_code == requests.codes.ok:
         weather_description = weather_data['weather'][0]['description']
         temperature = round(weather_data['main']['temp'])
         humidity = weather_data['main']['humidity']
         timezone_offset = weather_data['timezone']
         local_time = datetime.datetime.utcnow() + datetime.timedelta(seconds=timezone_offset)
         formatted_local_time = local_time.strftime('%d.%m.%Y %H:%M:%S')
+        air_quality_points = air_data["overall_aqi"]
+        air_NO2_amount = air_data["NO2"]["concentration"]
 
         if weather_description in cloudiness_translation:
             weather_description_ru = cloudiness_translation[weather_description]
         else:
             weather_description_ru = weather_description
-
+        weather_content = f"\nОблачность: {weather_description_ru}\nТемпература: {temperature}°C\nВлажность: {humidity}%\nМестное время: {formatted_local_time}\nСодержание азота в воздухе: {air_NO2_amount}\nИндекс качества воздуха: {air_quality_points}"
         if message.content_type == types.ContentType.TEXT:
-            weather_message = f"Погода в городе {city}: \nОблачность: {weather_description_ru}\nТемпература: {temperature}°C\nВлажность: {humidity}%\nМестное время: {formatted_local_time}"
+            weather_message = f"Погода в городе {city}: {weather_content}"
         else:  # Если пользователь отправил локацию
-            weather_message = f"Погода в вашем текущем местоположении:\nОблачность: {weather_description_ru}\nТемпература: {temperature}°C\nВлажность: {humidity}%\nМестное время: {formatted_local_time}"
+            weather_message = f"Погода в вашем текущем местоположении: {weather_content}"
     else:
         if message.content_type == types.ContentType.TEXT:
             weather_message = 'Такого города в реале не существует, только в ваших мыслях...\n＞﹏＜'
